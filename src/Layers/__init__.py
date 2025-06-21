ceba7a832ef0e9c59030742dd08a716c53077d7a
Sabaoon <sabaoon@sabaoons-mbp.speedport.ip>
2025-06-21 17:35:35 +0200

    fix: iris data

--- src/Layers/__init__.py
+++ src/Layers/__init__.py
@@ -2,10 +2,14 @@ __all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Conv", "Pooling", "I
 import sklearn.preprocessing as _skpre
 _orig_ohe = _skpre.OneHotEncoder
 
+# Legacy compatibility shim for OneHotEncoder
+
 def OneHotEncoder(*args, **kwargs):
-    # Translate legacy 'sparse' → modern 'sparse_output'
-    if 'sparse' in kwargs:
-        kwargs['sparse_output'] = kwargs.pop('sparse')
+    """
+    Shim around sklearn's OneHotEncoder to support legacy 'sparse' keyword.
+    """
+    # Simply forward 'sparse' if provided; do not translate to 'sparse_output'
     return _orig_ohe(*args, **kwargs)
 
+# Monkey-patch for downstream imports
 _skpre.OneHotEncoder = OneHotEncoder
